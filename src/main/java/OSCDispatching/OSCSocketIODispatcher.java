0153baeb9c66ebe008c23a74894e60313356041f
caesarion <s.thiele87@yahoo.de>
2015-07-16 20:44:51 +0200

    [OSC Dispatcher] Added logger instead of system.out, Added parser for parsing the osc port given as a program argument

--- src/main/java/OSCDispatching/OSCSocketIODispatcher.java
+++ src/main/java/OSCDispatching/OSCSocketIODispatcher.java
@@ -2,6 +2,9 @@ package OSCDispatching;
 
 import java.net.DatagramPacket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
@@ -9,14 +12,19 @@ import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DisconnectListener;
 
 public class OSCSocketIODispatcher implements IOSCDispatcher {
-	private int port = 3333;
-	private OSCDatagramPort oscPort;
+
+	private static final Logger log = LoggerFactory.getLogger("OSCSocketIODispatcher");
+	
+	// the standard port is 3333
+	private int oscPort;
+	private OSCDatagramPort oscDatagramPort;
 	private SocketIOServer server;
 	private boolean startedIOServer = false;
 	private boolean connectedToOSC;
 
-	public OSCSocketIODispatcher() {
+	public OSCSocketIODispatcher(int oscPort) {
 		// initialize server
+		this.oscPort = oscPort;
 		Configuration config = new Configuration();
 		config.setHostname("localhost");
 		config.setPort(5000);
@@ -25,15 +33,15 @@ public class OSCSocketIODispatcher implements IOSCDispatcher {
 		server.addConnectListener(new ConnectListener() {
 
 			@Override
-			public void onConnect(SocketIOClient client) {				
-				System.out.println("client connected");
+			public void onConnect(SocketIOClient client) {
+				log.info("client connected");
 			}
 		});
 		server.addDisconnectListener(new DisconnectListener() {
 
 			@Override
 			public void onDisconnect(SocketIOClient client) {
-				System.out.println("client disconnected");
+				log.info("client disconnected");
 			}
 		});
 	}
@@ -41,8 +49,8 @@ public class OSCSocketIODispatcher implements IOSCDispatcher {
 	public void startSocketIOServer() {
 		if (server != null && !startedIOServer) {
 			server.start();
-			startedIOServer = true;
-			System.out.println("Started socket.io server");
+			startedIOServer = true;	
+			log.debug("Started socket.io server");
 		}
 	}
 
@@ -50,13 +58,13 @@ public class OSCSocketIODispatcher implements IOSCDispatcher {
 		if (server != null && startedIOServer) {
 			server.stop();
 			startedIOServer = false;
-			System.out.println("Stopped socket.io server");
+			log.debug("Stopped socket.io server");
 		}
 	}
 
 	public void dispatchOSCMessage(DatagramPacket packet) {
 		for (SocketIOClient client : server.getAllClients()) {
-			client.sendEvent("osc", packet);
+			client.sendEvent("osc", packet);			
 		}
 	}
 
@@ -66,12 +74,12 @@ public class OSCSocketIODispatcher implements IOSCDispatcher {
 	 */
 	public void connect() {
 		try {
-			oscPort = new OSCDatagramPort(port);
-			oscPort.addIOSCDispatcher(this);
-			oscPort.startListening();
+			oscDatagramPort = new OSCDatagramPort(oscPort);
+			oscDatagramPort.addIOSCDispatcher(this);
+			oscDatagramPort.startListening();
 			setConnectedToOSC(true);
 		} catch (Exception e) {
-			System.out.println("OSCDispatcher: failed to connect to port " + port);
+			log.error("OSCDispatcher: failed to connect to port " + oscPort);
 			setConnectedToOSC(false);
 		}
 	}
@@ -81,14 +89,15 @@ public class OSCSocketIODispatcher implements IOSCDispatcher {
 	 * port
 	 */
 	public void disconnect() {
-		oscPort.stopListening();
+		oscDatagramPort.stopListening();
 		try {
 			Thread.sleep(100);
 		} catch (Exception e) {
 		}
 		;
-		oscPort.close();
+		oscDatagramPort.close();
 		setConnectedToOSC(false);
+		log.info("Disconnected UDP connection to OSC-Server");
 	}
 
 	public boolean isConnectedToOSC() {
@@ -99,9 +108,30 @@ public class OSCSocketIODispatcher implements IOSCDispatcher {
 		this.connectedToOSC = connectedToOSC;
 	}
 
+	private static int parsePort(String... args) {
+		if (!(args.length == 2 && args[0].equals("-p"))) {
+			log.error("Mailformed program arguments");
+			return -1;
+		}
+		try {
+			return Integer.parseInt(args[1]);
+		} catch (NumberFormatException e) {
+			log.error("The port argument -p is not of type int!");
+			return -1;
+		}
+
+	}
+
 	public static void main(String[] args) throws InterruptedException {
-		OSCSocketIODispatcher oscdispatcher = new OSCSocketIODispatcher();
+		log.info("Try Starting OSC Dispatcher");
+		int oscPort = parsePort(args);
+		if (oscPort < 0) {
+			log.error("program arguments not valid. Shutdown server.");
+			return;
+		}
+		OSCSocketIODispatcher oscdispatcher = new OSCSocketIODispatcher(oscPort);
 		oscdispatcher.startSocketIOServer();
 		oscdispatcher.connect();
+		log.info("OSC Dispatcher Running..");
 	}
 }
